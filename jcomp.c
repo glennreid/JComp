41c224a96fa490c022b1e7055cd4fc5b6930c69c
Glenn Reid <github@glennreid.com>
2025-02-21 17:31:26 -0800

    Fixed some gnarly bugs
    recognizing anonymous functions like "function() { ... }" and not freaking out.

--- jcomp.c
+++ jcomp.c
@@ -10,7 +10,8 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <unistd.h>
-  #include <ftw.h>
+#define _XOPEN_SOURCE 500
+#include <ftw.h>
 
 char outfile[128];
 char *g_funcs[1024];
@@ -20,8 +21,9 @@ int idx_f = 0, idx_i = 0;
 short commentPlan = 1;		// 0 = keep, 1 = elide, 2 = drop
 short lineNumbers = 1;
 
+#define DIRECTORY FTW_D
 typedef struct FTW *sftwptr;
-typedef int (IterateProcPtr) (int (*p1)(const char *p2, const struct stat *p3, int p4, /*struct FTW *p5*/sftwptr p5), int p6, int p7);
+typedef int IterateProcPtr (const char *filename, const struct stat *statptr, int fileflags, struct FTW *pfwt);
 
 // function prototypes
 short is_reserved ( char *word );
@@ -29,12 +31,13 @@ int find_functions_and_globals ( char *infile, char *outfile );
 int process_file ( char *infile, char *outfile );
 unsigned long MMDoForAllFilesIn ( char *directory, IterateProcPtr apply_proc );
 short MMFilePathExists ( char *path );
-int process_js ( const char *fpath, const struct stat *sb, int tflag, sftwptr ftwbuf );
+int get_globals ( const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf );
+int process_js ( const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf );
 
 int main ( int argc, const char * argv[] ) {
-	int idx = 0, len = 0, result = 0;
-	char *infile = (char *)argv[1];
-	char *ext = "";
+	int result = 0;
+	//char *infile = (char *)argv[1];
+	//char *ext = "";
     char cwd[1024];
 
     getcwd(cwd, sizeof(cwd));
@@ -43,7 +46,10 @@ int main ( int argc, const char * argv[] ) {
 		getcwd(cwd, sizeof(cwd));
 		printf("Set working dir: %s\n", cwd);
 	}
-	MMDoForAllFilesIn ( "./js", (IterateProcPtr*)process_js );
+	// two-pass algorithm:
+	MMDoForAllFilesIn ( "./js", get_globals );
+	printf ( "find_functions_and_globals: %d functions\n", idx_f );
+	MMDoForAllFilesIn ( "./js", process_js );
 
 	return result;
 }
@@ -54,8 +60,10 @@ short is_reserved ( char *word )
 	int idx = 0;
 	char *reserved[] = {
 		"function", "if", "else", "for", "while", "return", "break", "continue", "in", "of",
+		"switch", "case",
 		"var", "let", "const", "true", "false",
-		"isNaN", "Object", "Math", "atob", "btoa", "parseInt", "parseFloat",
+		"isNaN", "Object", "Math", "atob", "btoa", "parseInt", "parseFloat", "Date",
+		"window", "document", "self", "this",
 		"typeof", "undefined", "null", "NULL",
 		NULL
 	};
@@ -78,11 +86,22 @@ int known_func ( char *name )
 			found = idx; break;
 		}
 	}
-	if ( found == -1 ) {
-		char *save = (char *)malloc ( strlen(name) );
+	return found;
+}
+
+int register_func ( char *name )
+{
+	int idx = 0, found = known_func ( name );
+	int len = 0;
+
+	if ( found == -1 && name ) {
+		len = (int)strlen ( name );
+		char *save = (char *)malloc ( len );
 		strcpy ( save, name );
 		g_funcs[idx_f] = save;
 		found = idx_f++;
+	} else {
+		short bp = 1;
 	}
 	return found;
 }
@@ -99,14 +118,32 @@ int known_ident ( char *name )
 			}
 		}
 	}
+	return found;
+}
+
+int register_ident ( char *name )
+{
+	int idx = 0, found = known_ident ( name );
+	int len = 0;
+	if ( name ) {
+		len = (int)strlen ( name );
+		for ( idx = 0; idx < idx_i; idx++ ) {
+			if ( !strcmp(g_idents[idx], name) ) {
+				found = idx; break;
+			}
+		}
+	}
 	if ( found == -1 ) {
 		if ( len ) {
 			char *save = (char *)malloc ( len );
 			strcpy ( save, name );
+			if ( name[0] == '1' ) {
+				short bp = 1;
+			}
 			g_idents[idx_i] = save;
 			found = idx_i++;
 		} else {
-			short bp = 1;
+			//short bp = 1;
 		}
 	}
 	return found;
@@ -126,36 +163,35 @@ short legal ( char ch )			// other chars allowed in identifiers, like _ and -
 int find_functions_and_globals ( char *infile, char *outfile )
 {
 	int result = 0;
-	int count_ch=0, count_lines=1, count_comm=0;
-	char ch = '\0', last = '\0', dot = '\0';
+	char ch = '\0', last = '\0';
 	FILE *fd = NULL;
-	char token[1024], func[1024], ident[1024]; // , comment[4096];
-	int func_idx=0, ident_idx=0;
-	int idx=0, cdx=0;
-    short in_comment=0, ctype=0, in_string=0, stype=0, ftoke=0, saw_slash=0, saw_dot=0;
-    short idb=0, cdb=0;		// debug flags
+	char token[1024];
+	int count_ch=0, count_lines=1, count_comm=0;
+	int func_idx=0;
+	int idx=0, lineCount = 1;
+	char line[1024];
+    short verbose = 0;
+	short in_comment=0, ctype=0, in_string=0, stype=0, ftoke=0, saw_slash=0, saw_dot=0;
 
 	fd = fopen ( infile, "r" );
-	if ( !fd ) {
-		printf ( "Cannot open %s\n", infile );
-		return -1;
-	}
+	if ( !fd ) { printf ( "Cannot open %s\n", infile ); return -1; }
+
 	while ( (ch = fgetc(fd)) != EOF ) {
 		short alpha = 0, end_comment = 0, end_string = 0;
-		count_ch++;
-		if ( cdx == 0 ) {
-			if ( !in_comment ) {
-				//printf ( "%00d: ", count_lines );
+
+		line[idx] = ch; line[idx+1] = '\0';
+		if ( !strcmp(infile,"./js/youtube.js") && ((lineCount == 88 || lineCount == 90) && ch == '(') ) {
+			if ( ftoke ) {
+				char *last = g_funcs[idx_f-1];
+				short bp = 1;
 			}
 		}
-		cdx++;
 		switch ( ch ) {
-			case '\r': case '\n':
-				count_lines++; cdx = 0;
-				break;
-			case '{': case '}': case '(': case ')': case '[': case ']' :
+			case '{': case '}': case '(': case ')': case '[': case ']':
+				ftoke = 0;
 				break;
 			case ';': case ':':
+				 ftoke = 0;
 				break;
 		}
 		if ( in_comment ) {
@@ -169,17 +205,16 @@ int find_functions_and_globals ( char *infile, char *outfile )
 			if ( ch == '/' ) {
 				if ( saw_slash ) {
 					in_comment = 1; ctype = 1; count_comm = 0;
-					if ( commentPlan < 2 ) //printf ( "/%c", ch );
+					//if ( commentPlan < 2 ) printf ( "/%c", ch );
 					saw_slash = 0;
 				} else {
 					saw_slash = 1;
 				}
-			} else {
-				//saw_slash = 0;
-			}
-			if ( ch == '*' && saw_slash ) {
+			} else if ( ch == '*' && saw_slash ) {
 				in_comment = 1; ctype = 2; count_comm = 0;
-				if ( commentPlan < 2 ) //printf ( "/%c", ch );
+				//if ( commentPlan < 2 ) printf ( "/%c", ch );
+				saw_slash = 0;
+			} else {
 				saw_slash = 0;
 			}
 		}
@@ -233,56 +268,32 @@ int find_functions_and_globals ( char *infile, char *outfile )
 		}
 		if ( idx == 0 ) {
 			if ( isalpha(ch) ) alpha = 1;	// first char must be pure alpha
-			if ( alpha && last == '.' ) {	// special case for things like str.length
-				saw_dot = 1;
-			}
 		} else {
 			if ( isalnum(ch) || legal(ch) ) alpha = 1;	// subsequent chars can be alphanumeric
 		}
+		if ( ch == '\n' ) lineCount++;
 		if ( alpha ) {
 			token[idx++] = ch;
 		} else if ( idx > 0 ) {				// end of token
-			short reserved = 0;
+			//short reserved = 0;
 			token[idx] = '\0';
-			reserved = is_reserved ( token );
-			if ( reserved || saw_dot ) {
-				//printf ( "%s", token );
-				if ( !strcmp(token, "function") ) ftoke = 1;
+			if ( !strcmp(token, "function") ) {
+				if ( !strcmp(infile,"./js/youtube.js") && lineCount == 88 ) {
+					short bp = 1;
+				}
+				ftoke = 1;
 			} else {
-				if ( strlen(token) > 0 ) {
-					if ( ftoke ) {
-						//strcpy ( func, token );		// is a function name
-						func_idx = known_func ( token );
-						//printf ( "%s[f%d]", token, func_idx );
-						printf ( "\tfunction %d: %s\n", func_idx, token );
-					} else {
-						//strcpy ( ident, token );	// is an identifier
-						//ident_idx = known_ident ( token );
-						//printf ( "%s_i%d", token, ident_idx );
+				if ( ftoke && strlen(token) > 0 ) {
+					if ( !strcmp(token, "window") ) {
+						short bp = 1;
 					}
-					//if ( ident_idx > 29 ) break;
+					func_idx = register_func ( token );
+					if ( verbose ) printf ( "\tfunction %d: %s\n", func_idx, token );
 				}
 				ftoke = 0;
 			}
 			idx = 0;
-		}
-		if ( isspace(ch) || !isalnum(ch) ) saw_dot = 0;
-
-		if ( !alpha ) {
-			if ( !saw_slash ) {
-				//if ( (!lineNumbers ) {
-					//printf ( "%c", ch );
-				//}
-			} else {
-				//if ( !in_comment )
-				//	printf ( "/" );
-			}
-		} else {
-			if ( saw_slash ) {
-				//if ( !in_comment && commentPlan < 2 )
-				//	printf ( "/" );
-				saw_slash = 0;
-			}
+			token[idx] = '\0';	// clear token
 		}
 		last = ch;
 	}
@@ -295,13 +306,13 @@ int process_file ( char *infile, char *outfile )
 {
 	int result = 0;
 	int count_ch=0, count_lines=1, count_comm=0;
-	char ch = '\0', last = '\0', dot = '\0';
+	char ch = '\0', last = '\0';
 	FILE *fd = NULL;
-	char token[1024], func[1024], ident[1024]; // , comment[4096];
+	char token[1024];
 	int func_idx=0, ident_idx=0;
 	int idx=0, cdx=0;
-    short in_comment=0, ctype=0, in_string=0, stype=0, ftoke=0, saw_slash=0, saw_dot=0;
-    short idb=0, cdb=0;		// debug flags
+	short in_comment=0, ctype=0, in_string=0, stype=0, ftoke=0, saw_slash=0, saw_dot=0;
+	//short idb=0, cdb=0;		// debug flags
 
 	printf ( "infile: %s\n", infile );
 	printf ( "outfile: %s\n", outfile );
@@ -326,8 +337,10 @@ int process_file ( char *infile, char *outfile )
 				count_lines++; cdx = 0;
 				break;
 			case '{': case '}': case '(': case ')': case '[': case ']' :
+				ftoke = 0;
 				break;
 			case ';': case ':':
+				ftoke = 0;
 				break;
 		}
 		if ( in_comment ) {
@@ -346,12 +359,11 @@ int process_file ( char *infile, char *outfile )
 				} else {
 					saw_slash = 1;
 				}
-			} else {
-				//saw_slash = 0;
-			}
-			if ( ch == '*' && saw_slash ) {
+			} else if ( ch == '*' && saw_slash ) {
 				in_comment = 1; ctype = 2; count_comm = 0;
 				if ( commentPlan < 2 ) printf ( "/%c", ch );
+				saw_slash = 0;
+			} else {
 				saw_slash = 0;
 			}
 		}
@@ -424,13 +436,18 @@ int process_file ( char *infile, char *outfile )
 				if ( strlen(token) > 0 ) {
 					if ( ftoke ) {
 						//strcpy ( func, token );		// is a function name
-						func_idx = known_func ( token );
+						func_idx = register_func ( token );
 						//printf ( "%s[f%d]", token, func_idx );
 						printf ( "%s", token );
 					} else {
 						//strcpy ( ident, token );	// is an identifier
-						ident_idx = known_ident ( token );
-						printf ( "%s_i%d", token, ident_idx );
+						func_idx = known_func ( token );
+						if ( func_idx >= 0 ) {		// don't rewrite this function name
+							printf ( "%s", token );
+						} else {
+							ident_idx = register_ident ( token );
+							printf ( "%s_i%d", token, ident_idx );
+						}
 					}
 					//if ( ident_idx > 29 ) break;
 				}
@@ -541,64 +558,46 @@ short MMFilePathExists ( char *path )
 	return exists;
 }
 
-int process_js ( const char *fpath, const struct stat *sb, int tflag, sftwptr ftwbuf )
+// int process_js ( const char *fpath, const struct stat *sb, int tflag, sftwptr ftwbuf )
+int get_globals ( const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf )
 {
     #pragma unused ( sb )
-	int idx;
-	int result;
-	char type[12], buff[1024], indent[128];
-	short reallyDelete = 1;
-	int level = (int)ftwbuf->level;
-	short shouldProcess = 1;
+	if ( tflag == DIRECTORY )  return 0;
 
-	indent[0] = '\0';
-	for ( idx = 0; idx < level; idx++ ) {
-		strcat ( indent, "." );
+	if ( strstr(fpath,".js") ) {
+		// printf ( "FILE: %s\n", fpath );
+		find_functions_and_globals ( (char *)fpath, "globals.js" );
 	}
+	return 0;           /* To tell nftw() to continue */
+}
 
-	if ( tflag == FTW_D ) {
-		if ( level < 1 ) {
-			fprintf ( stderr, "IGNORE: %s", (char *)fpath + ftwbuf->base );
-			return 0;
+int process_js ( const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf )
+{
+    #pragma unused ( sb )
+	if ( tflag == FTW_D ) {		// directory
+		return 0;
+	}
+	//char *filename = (char *)fpath + ftwbuf->base;
+	char *filename = (char *)fpath;
+	if ( strstr(filename,".js") ) {
+		printf ( "FILE: %s\n", filename );
+		char *infile = filename;
+		strcpy ( outfile, infile );
+		int len = (int)strlen ( outfile );
+		char *ext = (outfile + (len-3));
+		if ( strncmp(ext,".js",3) ) {
+			printf ( "Usage: jcomp <filename>.js\n" );
+			return -1;
 		}
-		shouldProcess = 0;		// there shouldn't be any directories in here!
+		*ext = '\0';	// chop extension
+		strcat ( outfile, "-out.js" );
+
+		process_file ( infile, outfile );
+
 	} else {
-		if ( level > 1 ) {
-			//shouldProcess = 0;
-		}
-	}
-	if ( shouldProcess ) {
-		//char *filename = (char *)fpath + ftwbuf->base;
-		char *filename = (char *)fpath;
-		if ( strstr(filename,".js") ) {
-			printf ( "FILE: %s\n", filename );
-			char *infile = filename;
-			strcpy ( outfile, infile );
-			int len = (int)strlen ( outfile );
-			char *ext = (outfile + (len-3));
-			if ( strncmp(ext,".js",3) ) {
-				printf ( "Usage: jcomp <filename>.js\n" );
-				return -1;
-			}
-			*ext = '\0';	// chop extension
-			strcat ( outfile, "-out.js" );
-
-			result = find_functions_and_globals ( infile, outfile );
-
-		} else {
-			// printf ( "Not JS file: %s\n", filename );
-		}
+		// printf ( "Not JS file: %s\n", filename );
 	}
 
-	if ( !shouldProcess ) {
-		sprintf ( buff, "PROCESS_JS DEEP FILE: %s | %s%s", type, indent, (char *)fpath + ftwbuf->base );
-		fprintf ( stderr, buff );
-		if ( reallyDelete ) {
-			//if ( unlink(fpath) ) {		// if it's a file, unlink it, else
-			//	rmdir(fpath);			// it's a directory, so rmdir it
-			//}
-		}
-	}
 	return 0;           /* To tell nftw() to continue */
 }
 
